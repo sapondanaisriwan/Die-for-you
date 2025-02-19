2792b8536785f98980b005ac2b42da1e721d0728
Kittipong <anonymous51124@gamil.com>
2025-02-19 15:54:30 +0700

    push

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,7 @@ int main()
     InitWindow(screenWidth, screenHeight, "Die for you");
     SetTargetFPS(60);
 
+    // homepage
     Button topbar{"img/homepage/topbar.png", {0, 0}, 1};
     Button newDesk{"img/homepage/new-desk.png", {48, 95}, 1};
     Button foodBtn{"img/homepage/Foods.png", {278.12, 95}, 1};
@@ -33,32 +34,47 @@ int main()
     Button jobsBtn{"img/homepage/jobs.png", {278.12, 384}, 1};
     Button vegetableBtn{"img/homepage/vegetable.png", {508.24, 384}, 1};
 
+    // gameplay
+    Button gameplayBG{"img/gameplay/bg.png", {48, 48}, 1};
+    Button gmaeplayhome{"img/gameplay/home-btn.png", {64, 650}, 1};
+    Button gameplaynext{"img/gameplay/next-btn.png", {528, 649}, 1};
+    Button gameplayprevious{"img/gameplay/previous-btn.png", {472, 687}, 1};
+    Button gameplayans{"img/gameplay/show-answer-btn.png", {809, 625}, 1};
+
+
     while (!WindowShouldClose())
     {
 
-        Vector2 mousePosition = GetMousePosition();
-        bool mousePressed = IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
+        // Vector2 mousePosition = GetMousePosition();
+        // bool mousePressed = IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
 
         BeginDrawing();
+
+        gameplayBG.Draw();
+        gmaeplayhome.Draw();
+        gameplaynext.Draw();
+        gameplayprevious.Draw();
+        gameplayans.Draw();
+
         ClearBackground(RAYWHITE);
-        if (currentWindow == HOME_WINDOW)
-        {
-            if (foodBtn.isPressed(mousePosition, mousePressed))
-            {
-                currentWindow = GAMEPLAY_WINDOW;
-            }
-            topbar.Draw();
-            newDesk.Draw();
-            foodBtn.Draw();
-            animalBtn.Draw();
-            flowerBtn.Draw();
-            countryBtn.Draw();
-            jobsBtn.Draw();
-            vegetableBtn.Draw();
-        }
-        else if (currentWindow == GAMEPLAY_WINDOW)
-        {
-        }
+        // if (currentWindow == HOME_WINDOW)
+        // {
+        //     if (foodBtn.isPressed(mousePosition, mousePressed))
+        //     {
+        //         currentWindow = GAMEPLAY_WINDOW;
+        //     }
+        //     topbar.Draw();
+        //     newDesk.Draw();
+        //     foodBtn.Draw();
+        //     animalBtn.Draw();
+        //     flowerBtn.Draw();
+        //     countryBtn.Draw();
+        //     jobsBtn.Draw();
+        //     vegetableBtn.Draw();
+        // }
+        // else if (currentWindow == GAMEPLAY_WINDOW)
+        // {
+        // }
         EndDrawing();
     }
 
