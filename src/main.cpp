b65dd3edc027bfaa02aa3fdd3b28c4ee27b73c6b
Sapondanai Sriwan <sapondanaisriwan@gmail.com>
2025-02-19 15:35:37 +0700

    Add Button class for UI interaction with image assets

--- src/main.cpp
+++ src/main.cpp
@@ -1,113 +1,65 @@
 #include "raylib.h"
+#include <iostream>
+#include "button.hpp"
 
-Color purple600 = {119, 49, 216, 255};
-Color purple500 = {101, 20, 213, 255};
+using namespace std;
 
-class Button
+enum WindowState
 {
-public:
-    Rectangle rect;
-    Color color;
-    bool clicked;
-
-    Button(Rectangle rect, Color color)
-    {
-        this->rect = rect;
-        this->clicked = false;
-        this->color = color;
-    }
-
-    void draw()
-    {
-        DrawRectangleRec(rect, color);
-    }
-
-    void draw2()
-    {
-        DrawRectangleRounded(rect, 0.3, 0, color);
-    }
-
-    void drawText(const char *text, int fontSize)
-    {
-        // Center text on button rectangle using text width and height
-        int textWidth = MeasureText(text, fontSize);
-        int textHeight = fontSize;
-
-        float xCentered = rect.x + (rect.width - textWidth) / 2;
-        float yCentered = rect.y + (rect.height - textHeight) / 2;
-        DrawText(text, xCentered, yCentered, fontSize, WHITE);
-    }
-
-    void update()
-    {
-        if (CheckCollisionPointRec(GetMousePosition(), rect))
-        {
-            color = purple500;
-            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-            {
-                clicked = true;
-            }
-        }
-        else
-        {
-            color = purple600;
-        }
-    }
+    HOME_WINDOW,
+    GAMEPLAY_WINDOW
 };
+WindowState currentWindow = HOME_WINDOW;
 
-// Button deckBtn = Button({259, 100, 150, 50}, purple600);
-// Button addBtn = Button({425, 100, 150, 50}, purple600);
-// Button browseBtn = Button({591, 100, 150, 50}, purple600);
-
-// deckBtn.draw2();
-// deckBtn.drawText("H", 20);
-// deckBtn.update();
-
-// addBtn.draw2();
-// addBtn.drawText("Add", 20);
-// addBtn.update();
-
-// browseBtn.draw2();
-// browseBtn.drawText("Browse", 20);
-// browseBtn.update();
+void homepage(Texture2D textures[], Vector2 positions[])
+{
+}
 
 int main()
 {
     // Initialize window
     const float screenWidth = 1000;
-    const float screenHeight = 750; 
+    const float screenHeight = 750;
 
     InitWindow(screenWidth, screenHeight, "Die for you");
     SetTargetFPS(60);
 
-    Texture2D textures[] = {
-        LoadTexture("img/firstpage/topbar.png"),
-        LoadTexture("img/firstpage/new-desk.png"),
-        LoadTexture("img/firstpage/Foods.png"),
-        LoadTexture("img/firstpage/animals.png"),
-        LoadTexture("img/firstpage/flowers.png"),
-        LoadTexture("img/firstpage/country.png"),
-        LoadTexture("img/firstpage/jobs.png"),
-        LoadTexture("img/firstpage/vegetable.png"),
-    };
-
-    Vector2 positions[] = {
-        {0, 0}, {48, 95}, {278.12, 95}, {508.24, 95}, {738.36, 95}, {48, 384}, {278.12, 384}, {508.24, 384}};
+    Button topbar{"img/homepage/topbar.png", {0, 0}, 1};
+    Button newDesk{"img/homepage/new-desk.png", {48, 95}, 1};
+    Button foodBtn{"img/homepage/Foods.png", {278.12, 95}, 1};
+    Button animalBtn{"img/homepage/animals.png", {508.24, 95}, 1};
+    Button flowerBtn{"img/homepage/flowers.png", {738.36, 95}, 1};
+    Button countryBtn{"img/homepage/country.png", {48, 384}, 1};
+    Button jobsBtn{"img/homepage/jobs.png", {278.12, 384}, 1};
+    Button vegetableBtn{"img/homepage/vegetable.png", {508.24, 384}, 1};
 
     while (!WindowShouldClose())
     {
-        // Start drawing
+
+        Vector2 mousePosition = GetMousePosition();
+        bool mousePressed = IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        for (int i = 0; i < 8; i++)
+        if (currentWindow == HOME_WINDOW)
         {
-            DrawTexture(textures[i], positions[i].x, positions[i].y, WHITE);
+            if (foodBtn.isPressed(mousePosition, mousePressed))
+            {
+                currentWindow = GAMEPLAY_WINDOW;
+            }
+            topbar.Draw();
+            newDesk.Draw();
+            foodBtn.Draw();
+            animalBtn.Draw();
+            flowerBtn.Draw();
+            countryBtn.Draw();
+            jobsBtn.Draw();
+            vegetableBtn.Draw();
+        }
+        else if (currentWindow == GAMEPLAY_WINDOW)
+        {
         }
         EndDrawing();
-    }
-    for (int i = 0; i < 8; i++)
-    {
-        UnloadTexture(textures[i]);
     }
 
     CloseWindow();
